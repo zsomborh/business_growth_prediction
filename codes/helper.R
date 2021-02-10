c7a8b6eea9f52676d98a17e657a9af93d36a6dc8
Zsombor Hegedus <zsomborhegedus@gmail.com>
2021-02-10 22:05:38 +0100

    add helper

---
+++ codes/helper.R
@@ -0,0 +1,18 @@
+################################################################################################
+# Prepared for the textbook:
+# Data Analysis for Business, Economics, and Policy
+# by Gabor BEKES (Central Europen University) and  Gabor KEZDI (University of Michigan)
+# Cambridge University Press 2020
+
+# License: Free to share, modify and use for educational purposes. 
+# Not to be used for business purposes
+# 
+#
+###############################################################################################x
+
+twoClassSummaryExtended <- function (data, lev = NULL, model = NULL)
+{
+    lvls <- levels(data$obs)
+    rmse <- sqrt(mean((data[, lvls[1]] - ifelse(data$obs == lev[2], 0, 1))^2))
+    c(defaultSummary(data, lev, model), "RMSE" = rmse)
+}
